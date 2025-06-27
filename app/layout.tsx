d861696841f1d75d189ee1ed479734ac7b3fa2c6
Sparsh Taparia <155702560+Sparshtaparia@users.noreply.github.com>
2025-06-27 23:13:39 +0530

    layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,9 @@ export const metadata: Metadata = {
   title: 'Capify',
   description: 'Created by Sparsh',
   generator: 'Sparsh',
+  icons: {
+    icon: '/Screenshot 2025-06-27 230957.png', // or '/logo.png' for PNG
+  },
 }
 
 export default function RootLayout({
