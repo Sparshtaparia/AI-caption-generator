47f781a123b32cbb4f0280d0fe1e470c02af6a81
Sparsh Taparia <155702560+Sparshtaparia@users.noreply.github.com>
2025-06-27 23:08:30 +0530

    layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'Capify',
+  description: 'Created by Sparsh',
+  generator: 'Sparsh',
 }
 
 export default function RootLayout({
